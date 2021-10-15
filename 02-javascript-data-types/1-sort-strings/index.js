43c0822a451bb23e0969e0739cc31f3f1074ac00
Maxim Myakishev <amaxbn@gmail.com>
2021-10-15 09:02:45 +0300

    Отработал коменты

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -7,15 +7,24 @@
 export function sortStrings(arr, param = 'asc') {
   const locale = ['ru', 'en'];
   const upper = {caseFirst: 'upper'};
+  const diretions = {
+    asc: 1,
+    desc: -1
+  }
+  const direction = diretions[param];
 
-  if (param === 'asc') {
-    return [...arr].sort((a, b) =>
-      a.localeCompare(b, locale, upper)
-    );
-  }
-  if (param === 'desc') {
-    return [...arr].sort((a, b) =>
-      b.localeCompare(a, locale, upper)
-    );
-  }
+  // if (param === 'asc') {
+  //   return [...arr].sort((a, b) =>
+  //     a.localeCompare(b, locale, upper)
+  //   );
+  // }
+  // if (param === 'desc') {
+  //   return [...arr].sort((a, b) =>
+  //     b.localeCompare(a, locale, upper)
+  //   );
+  // }
+
+  return [...arr].sort((a, b) => {
+    return diretion * a.localeCompare(b, locale, upper);
+  });
 }
--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -6,7 +6,7 @@
  */
 export const omit = (obj, ...fields) => {
 
-  let result = {};
+  const result = {};
 
   for (const [key, value] of Object.entries(obj)) {
     if (!fields.includes(key)) {
